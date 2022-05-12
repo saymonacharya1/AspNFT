8599e68d17c868ec8241e702a2cc288d7b781227
Saymon Acharya <acharyasaymon@gmail.com>
2022-05-12 18:00:33 +0545

    added 2 sections

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+ if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+        $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+
+
 const wrapperMenu = document.querySelector('.wrapperMenu');
 const menuItems = document.querySelector('.menu_items');
 
@@ -10,3 +25,26 @@ document.querySelectorAll(".item").forEach(n => n. addEventListener("click", () 
     wrapperMenu.classList.remove("active");
     menuItems.classList.remove("active");
 } ))
+
+
+var win = $(window);
+var allMods = $(".module");
+
+// Already visible modules
+allMods.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible"); 
+  } 
+});
+
+win.scroll(function(event) {
+  
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass(".box"); 
+    } 
+  });
+  
+});
