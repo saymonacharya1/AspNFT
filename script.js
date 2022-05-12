8054814677b203339d03fde6547cb066c31a7451
Saymon Acharya <acharyasaymon@gmail.com>
2022-05-12 18:11:45 +0545

    bg changed

--- script.js
+++ script.js
@@ -27,24 +27,3 @@ document.querySelectorAll(".item").forEach(n => n. addEventListener("click", () 
 } ))
 
 
-var win = $(window);
-var allMods = $(".module");
-
-// Already visible modules
-allMods.each(function(i, el) {
-  var el = $(el);
-  if (el.visible(true)) {
-    el.addClass("already-visible"); 
-  } 
-});
-
-win.scroll(function(event) {
-  
-  allMods.each(function(i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-      el.addClass(".box"); 
-    } 
-  });
-  
-});
